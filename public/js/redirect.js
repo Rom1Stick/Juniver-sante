4d2a0eae0ef9a61a8434ccb044515cd09ede38b6
Alexis <alexis.magnier2001@gmail.com>
2024-11-29 16:29:52 +0100

    set up

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -2,5 +2,7 @@
   // Si l'URL contient "invite_token" ou "confirmation_token", redirige vers /admin
   if (window.location.hash.includes("invite_token") || window.location.hash.includes("confirmation_token")) {
       window.location.href = "/admin/";
+      console.log("Invite token détecté, redirection en cours...");
+
   }
 })();
